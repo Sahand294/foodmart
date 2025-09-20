82a17b4af0f6e8280c4c5e60c50b88f6e96c7a8d
jazavaja <63429229+jazavaja@users.noreply.github.com>
2025-09-20 22:50:48 +0330

    Update address.py

--- models/address.py
+++ models/address.py
@@ -1,29 +1,48 @@
-from . import db,u
+from . import db
 import datetime
+
 class Country(db.Model):
     __tablename__ = 'country'
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(30))
-    key = db.Column(db.String(200),unique=True)
-
-class City(db.Model):
-    __tablename__ = 'city'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30))
-    key = db.Column(db.String(200),unique=True)
-    province = db.Column(db.Integer,db.ForeignKey('provinces.id'))
+    key = db.Column(db.String(200), unique=True)
+
+    provinces = db.relationship(
+        'ProvinceOrTerritories',
+        backref='country_obj',
+        cascade='all, delete-orphan'
+    )
 
 class ProvinceOrTerritories(db.Model):
     __tablename__ = 'provinces'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
-    country = db.Column(db.Integer,db.ForeignKey('country.id'))
+    country = db.Column(db.Integer, db.ForeignKey('country.id', ondelete='CASCADE'))
+
+    cities = db.relationship(
+        'City',
+        backref='province_obj',
+        cascade='all, delete-orphan'
+    )
+
+class City(db.Model):
+    __tablename__ = 'city'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    key = db.Column(db.String(200), unique=True)
+    province_id = db.Column(db.Integer, db.ForeignKey('provinces.id', ondelete='CASCADE'))
+
+    addresses = db.relationship(
+        'Address_User',
+        backref='city_obj',
+        cascade='all, delete-orphan'
+    )
 
 class Address_User(db.Model):
     __tablename__ = 'address_user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30))
-    city = db.Column(db.Integer,db.ForeignKey('city.id'))
+    city_id = db.Column(db.Integer, db.ForeignKey('city.id', ondelete='CASCADE'))
     address = db.Column(db.String(999))
     postal_code = db.Column(db.String(999))
-    User = db.Column(db.Integer,db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'))
