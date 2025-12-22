1e65425cafaeffa5e07a8b5d14b7c09445b29644
farza_mggxnah <sahandha1391@gmail.com>
2025-12-22 20:12:18 -0330

    updating and trying to fix a bug inside of when trying to add address id to orders

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:sahand91@127.0.0.1:5432/FOODMARTSTORE'
+    SQLALCHEMY_DATABASE_URI = "postgresql://foodmart_user:sXqBgnz6j1eSk3b1iRlQ2qW6D8cJWOkz@dpg-d54tdgp5pdvs73bpl2eg-a/foodmart"
