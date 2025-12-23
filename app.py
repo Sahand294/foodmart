ed775f4d23fcbfb79fbe4a7e5f213608f0bbb2b5
farza_mggxnah <sahandha1391@gmail.com>
2025-12-22 21:57:07 -0330

    updating and trying to fix a bug inside of when trying to add address id to orders

--- app.py
+++ app.py
@@ -916,8 +916,13 @@ def cancel():
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    AddAccounts.add(firstname='sahand', lastname='hajiabadi', password='12345sahand', email='sahandha1391@gmail.com',
-                    username='loki1391', role='Owner')
+    # roleid = Roles.query.filter_by(name=role).first()
+    print('created')
+    new_password = AddAccounts.encrypting_password('sahand91')
+    user = Users(firstname='sahand', lastname='hajiabadi', username='loki1391', email='sahandha1391@gmail.com',
+                 password=new_password, roleid=1)
+    db.session.add(user)
+    db.session.commit()
     if 'role' not in session:
         session['role'] = 'guest'
     if 'logged' not in session:
